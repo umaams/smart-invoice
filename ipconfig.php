126bba1bcac554d9d257cc2f1b9b0613d60b2e66
Khotibul Umam <khotib.umam7@gmail.com>
2025-12-12 08:59:36 +0700

    Update ipconfig.php

--- ipconfig.php
+++ ipconfig.php
@@ -5,7 +5,7 @@
 # Set your URL without trailing slash here, e.g. http://your-domain.com
 # If you use a subdomain, use http://subdomain.your-domain.com
 # If you use a subfolder, use http://your-domain.com/subfolder
-IP_URL="https://billing.smartone.web.id"
+IP_URL="https://invoice.smartone.co.id"
 
 # Having problems? Enable debug by changing the value to 'true' to enable advanced logging
 ENABLE_DEBUG=true
